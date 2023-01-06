da4d67bf3b1ede3a6cb4a3bdab842a8894551b81
juanj72 <juan.jara@campusucc.edu.co>
2023-01-06 14:18:54 -0500

    primer commit del curos vue js

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <hr>
+
+  <MenuInicio msg="hola desde mi componente"/>
+  <hr>
+  
+
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MenuInicio from './components/MenuInicio.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MenuInicio
+    
   }
 }
 </script>
@@ -22,5 +34,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+hr{
+  border-top: 1px solid rgb(187, 15, 15);
 }
 </style>
