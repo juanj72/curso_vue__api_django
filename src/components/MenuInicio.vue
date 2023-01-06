1deaf16ced52b61462cc603c4041c31302d2e23e
juanj72 <juan.jara@campusucc.edu.co>
2023-01-06 14:19:32 -0500

    commit de comparacion para los inicios en vue js

---
+++ src/components/MenuInicio.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <h2>hola mundo desde otro component</h2>
+
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis ea distinctio, repellendus, expedita deleniti totam tempore error quisquam aut eaque iusto consequuntur ut neque quia doloribus velit odio quidem dignissimos!</p>
+</template>
+
+<script>
+
+export default {
+  name: 'MenuInicio',
+  props: {
+    msg: String
+  }
+}
+
+</script>
+
+
+
+<style scoped>
+h2{
+    color: aqua;
+}
+p{
+    color: yellow;
+}
+</style>
