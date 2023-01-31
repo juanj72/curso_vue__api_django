6d25f9e4e82537ab9adb93d24bbdfffd64231504
juanj72 <juan.jara@campusucc.edu.co>
2023-01-31 18:06:32 -0500

    django conectado mediante API

---
+++ src/components/Componente_prueba.vue
@@ -0,0 +1,38 @@
+<template>
+<h2>prueba de componentes en vue</h2>
+
+<p>programacion de frontend en mi camino a un nuevo trabajo</p>
+<div v-for="post in APIData" :key="post.id">
+<p>{{ post.title }}</p>
+<p>{{ post.content }}</p>
+<hr>
+</div>
+
+</template>
+
+<script>
+
+import {getAPI} from '../axios-api'
+
+  export default{
+    name:'Componente_prueba',
+    props:{
+        msg:String
+    },
+    data(){
+      return{
+        APIData:[]
+      }
+    },
+    created(){
+      getAPI.get('/first_app/post_api2',).then(response=>{
+        console.log('post API has received data')
+        this.APIData=response.data
+
+
+      }).catch(err=>{
+        console.log(err)
+      })
+    }
+  }
+</script>
---
+++ src/components/Otro_si.vue
@@ -0,0 +1,17 @@
+<template>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque unde accusamus ipsam, alias, ea odit sunt minima, maxime non quo voluptas quae? Doloremque, ut quidem. Ullam unde dolor eius officia!</p>
+
+</template>
+
+
+
+
+<script>
+
+
+
+export default {
+  name: 'otro_si'
+  
+}
+</script>
